14d3f2dd3b2f2fd8f3f3551e7986ce971840027e
fnanfne <fnanfne@gmail.com>
2023-07-17 02:28:47 +0100

    Worked slightly on the Pinkfly's movement

--- Scenes/Pinkfly/pinkfly.gd
+++ Scenes/Pinkfly/pinkfly.gd
@@ -9,13 +9,13 @@ func _ready():
 	get_node("AnimatedSprite2D").play("Fly")
 
 func _physics_process(delta):
-	print(velocity.x)
+	print(delta)
 	var found_wall = is_on_wall()
 	if found_wall:
 		direction *= -1
 		$Timer.start(0.0005)
 		$Timer2.start(1)
-	velocity.x += direction * MAX_SPEED * delta
+	velocity.x += direction * MAX_SPEED * (delta * 1.5)
 	if direction > 0:
 		get_node("AnimatedSprite2D").flip_h = false
 		$WallCheck.position = (Vector2(17,0))
