b086eb89861738034cdd927c9221e6206e05d7ff
fnanfne <fnanfne@gmail.com>
2023-07-17 02:22:08 +0100

    Near perfected the Pinkfly's animation and movement!

--- Scenes/Pinkfly/pinkfly.gd
+++ Scenes/Pinkfly/pinkfly.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var SPEED = 200
+var MAX_SPEED = 300
 var direction = 1
 
 @onready var wallCheck = $WallCheck
@@ -8,13 +8,14 @@ var direction = 1
 func _ready():
 	get_node("AnimatedSprite2D").play("Fly")
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	print(velocity.x)
 	var found_wall = is_on_wall()
 	if found_wall:
 		direction *= -1
 		$Timer.start(0.0005)
 		$Timer2.start(1)
-	velocity.x = direction * SPEED
+	velocity.x += direction * MAX_SPEED * delta
 	if direction > 0:
 		get_node("AnimatedSprite2D").flip_h = false
 		$WallCheck.position = (Vector2(17,0))
@@ -24,9 +25,10 @@ func _physics_process(_delta):
 	move_and_slide()
 
 func _on_timer_timeout():
-	SPEED = 0
+	MAX_SPEED = 0
+	velocity.x = 0
 	get_node("AnimatedSprite2D").play("Idle")
 
 func _on_timer_2_timeout():
-	SPEED = 200
+	MAX_SPEED = 300
 	get_node("AnimatedSprite2D").play("Fly")
