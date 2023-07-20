dc6a03320dbcaf736732cea190fa47101b7755f9
fnanfne <fnanfne@gmail.com>
2023-07-20 03:14:28 +0100

    Worked on modulating the teleporter

--- Scenes/Telematics/teleporter.gd
+++ Scenes/Telematics/teleporter.gd
@@ -22,3 +22,13 @@ func _physics_process(_delta):
 	if teleporter_active == true:
 		if is_ready_to_teleport == true and Input.is_action_just_pressed("jump"):
 			player.position = target.position
+	var TW1 = get_tree().create_tween()
+	#TW1.tween_interval(5)
+	TW1.tween_property($Node2D/On_Top, "modulate:a", 
+	0.0, 3.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	#TW1.tween_interval(8)
+	TW1.tween_property($Node2D/On_Top, "modulate:a", 
+	5.0, 1.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	TW1.tween_property($Node2D/On_Top, "modulate:a",
+	Color(1,1,1,1), 3)
+	#TW1.tween_property($Sprite2D, "position", position - $Robot.get_node(position.x), 0.8)
