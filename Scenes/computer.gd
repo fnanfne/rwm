d73f5c866147533af656a359f39e28981f9d7fbd
fnanfne <fnanfne@gmail.com>
2023-08-12 05:11:07 +0100

    Added working Haxxors, not in a system, but working

--- Scenes/computer.gd
+++ Scenes/computer.gd
@@ -1,4 +1,29 @@
 extends Area2D
 
 @export var link_code : int
-signal sad_happy()
+signal sad_happy(door)
+
+func _ready():
+	play_randomized("sad")
+
+func play_randomized(sad : String):
+	randomize()
+	$AnimationPlayer.play(sad)
+	var offset : float = randf_range(0, $AnimationPlayer.current_animation_length)
+	$AnimationPlayer.advance(offset)
+
+func _on_body_entered(body):
+		if body.is_in_group("Robots"):
+			#sad_happy()
+			$Case/Sad.hide()
+			$Case/SadBlink.hide()
+			$AnimationPlayer.play("happy")
+			#yield($AnimationPlayer, "animation_finished")
+			$SadHappy.play()
+			#$DoorOpen.play()
+			$CollisionShape2D.queue_free()
+			emit_signal("sad_happy", link_code)
+
+func _on_sad_happy():
+	#$AnimationPlayer.play("happy")
+	pass
