d0642884ee5770fa0b9022857d420805a4e7f0ed
fnanfne <fnanfne@gmail.com>
2023-09-21 01:55:16 +0100

    Level making
    Working on the level called "Disarmed"

---
+++ Levels/disarmed.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Game.starting_position = Vector2 (2860,1730)
+	#starting_position : = Vector2 (460,710)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
