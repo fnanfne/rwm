38ac86e4a03f871671978b6dffa042e085ade152
fnanfne <fnanfne@gmail.com>
2023-11-07 16:35:33 +0000

    Fixed the teleporter and started adding music .ogg files

--- Apps/radio.gd
+++ Apps/radio.gd
@@ -10,7 +10,7 @@ func _ready():
 	$MarginContainer/Label.text = text_field
 
 
-func _process(delta):
+func _process(_delta):
 	pass
 
 
--- Scenes/music_block.gd
+++ Scenes/music_block.gd
@@ -3,32 +3,24 @@ extends Area2D
 @export var song_beep : AudioStreamOggVorbis
 
 @onready var music_blocks = get_tree().get_nodes_in_group("music_blocks")
-var already_playing = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#print(music_blocks)
-	pass
+#var already_playing = false
 
 
 func _on_body_entered(body):
 	if body.is_in_group("Robots"):
-		if Game.is_music_playing:
-			pass
-		else:
-			for muzak in music_blocks:
-				muzak.stop()
-				print("ALL MUSIC SHOULD STOP!!!")
-			Game.is_music_playing = false
-			$Timer.start()
+		#if Game.is_music_playing:
+		#	print("PASSING< NO CHANGE!!")
+		#	pass
+		#else:
+		for muzak in music_blocks:
+			muzak.stop()
+			#print("ALL MUSIC SHOULD STOP!!!")
+		#Game.is_music_playing = false
+		$Timer.start()
 
 
 func _on_timer_timeout():
+		#print("NEW SONG SHOULD START!!")
 		$AudioStreamPlayer.stream = song_beep
 		$AudioStreamPlayer.play()
-		Game.is_music_playing = true
+		#Game.is_music_playing = true
